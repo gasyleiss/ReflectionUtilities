a61a2b4107f21b61169062e14a11f95209397a83
Alexander Goida <alexander.goida@gmail.com>
2015-07-20 14:09:25 +0300

    Initial code

---
+++ ReflectionUtilities/TypeExtensions.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Reflection;
+
+namespace ReflectionUtilities
+{
+    public static class TypeExtensions
+    {
+        private static MethodInfo GetMethod(Expression body)
+        {
+            MethodCallExpression method = body as MethodCallExpression;
+            if (method != null) return method.Method;
+
+            throw new ArgumentException("Not a method");
+        }
+
+        #region Invoke
+
+        public static void Invoke<TType>(this TType obj, string name, params object[] parameters) where TType : class
+        {
+            MethodInfo mi = typeof (TType).GetInfo(name, parameters);
+            if (mi != null)
+            {
+                mi.Invoke(obj, parameters);
+                return;
+            }
+
+            throw new ArgumentException(string.Format("The member '{0}' is not a method.", name));
+        }
+
+        public static TResult Invoke<TType, TResult>(this TType obj, string name, params object[] parameters) where TType : class
+        {
+            MethodInfo mi = typeof (TType).GetInfo(name, parameters);
+            if (mi != null)
+                return (TResult) mi.Invoke(obj, parameters);
+
+            throw new ArgumentException(string.Format("The member '{0}' is not a method.", name));
+        }
+
+        #endregion
+
+        #region Name
+
+        public static string GetName<TType>(this Type type, Expression<Action<TType>> action) where TType : class
+        {
+            return typeof (TType).GetInfo(action).Name;
+        }
+
+        public static string GetName<TType, TResult>(this Type type, Expression<Func<TType, TResult>> action) where TType : class
+        {
+            return typeof (TType).GetInfo(action).Name;
+        }
+
+        #endregion
+
+        #region Info
+
+        public static MethodInfo GetInfo<TType>(this Type type, Expression<Action<TType>> action) where TType : class
+        {
+            return GetMethod(action.Body);
+        }
+
+        public static MethodInfo GetInfo<TType, TResult>(this Type type, Expression<Func<TType, TResult>> action) where TType : class
+        {
+            return GetMethod(action.Body);
+        }
+
+        public static MethodInfo GetInfo(this Type type, string name, params object[] parameters)
+        {
+            if (parameters.Length == 0)
+                return type.GetMethod(name, new Type[] {});
+
+            Type[] types = parameters.Select(p => p.GetType()).ToArray();
+            return type.GetMethod(name, types);
+        }
+
+        #endregion
+    }
+}
---
+++ ReflectionUtilities/TypeExtensionsTests.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Reflection;
+using Xunit;
+
+namespace ReflectionUtilities
+{
+    public class TypeExtensionsTests
+    {
+        [Fact]
+        public void Should_ReturnMethodInfo()
+        {
+            MethodInfo mi = typeof (TestType).GetInfo<TestType>(m => m.PublicOverloadedMethod());
+            Assert.NotNull(mi);
+            // mi.GetType() != typeof(MethodInfo) because it's RuntimeMethodInfo class which is inherited and internal
+            Assert.True(mi.GetType().IsSubclassOf(typeof (MethodInfo)));
+        }
+
+        [Fact]
+        public void Should_ReturnInfo_ForSpecificParameters()
+        {
+            MethodInfo mi1 = typeof (TestType).GetInfo<TestType>(m => m.PublicOverloadedMethod(int.MinValue));
+            MethodInfo mi2 = typeof (TestType).GetInfo<TestType>(m => m.PublicOverloadedMethod(int.MinValue, string.Empty));
+            Assert.NotNull(mi1);
+            Assert.NotNull(mi2);
+            // should be different metadata elements
+            Assert.NotEqual(mi1.MetadataToken, mi2.MetadataToken);
+        }
+
+        [Fact]
+        public void Should_ReturnInfo_ByName()
+        {
+            MethodInfo mi = typeof (TestType).GetInfo("PublicOverloadedMethod");
+            Assert.NotNull(mi);
+        }
+
+        [Fact]
+        public void Should_ReturnInfo_ByName_ForSpecificParameters()
+        {
+            MethodInfo mi1 = typeof (TestType).GetInfo("PublicOverloadedMethod");
+            MethodInfo mi2 = typeof (TestType).GetInfo("PublicOverloadedMethod", int.MinValue, string.Empty);
+            Assert.NotNull(mi1);
+            Assert.NotNull(mi2);
+            // should be different metadata elements
+            Assert.NotEqual(mi1.MetadataToken, mi2.MetadataToken);
+        }
+
+        [Fact]
+        public void Should_ReturnName()
+        {
+            string name = typeof (TestType).GetName<TestType>(m => m.PublicOverloadedMethod());
+            Assert.Equal("PublicOverloadedMethod", name);
+        }
+
+        [Fact]
+        public void Should_ReturnName_WithParameters()
+        {
+            string name = typeof (TestType).GetName<TestType>(m => m.PublicOverloadedMethod(int.MinValue, string.Empty));
+            Assert.Equal("PublicOverloadedMethod", name);
+        }
+
+        [Fact]
+        public void Can_Invoke_Method_WithOut_Parameters()
+        {
+            TestType testObj = new TestType();
+            string name = typeof (TestType).GetName<TestType>(m => m.PublicOverloadedMethod());
+            testObj.Invoke(name);
+            Assert.True(testObj.Flag);
+        }
+
+        [Fact]
+        public void Can_Invoke_Method_With_Parameters()
+        {
+            TestType testObj = new TestType();
+            string name = typeof (TestType).GetName<TestType>(m => m.PublicOverloadedMethod());
+            testObj.Invoke(name, int.MinValue, string.Empty);
+            Assert.True(testObj.Flag);
+        }
+
+        [Fact]
+        public void Can_Invoke_Method_With_Result()
+        {
+            TestType testObj = new TestType();
+            string name = typeof (TestType).GetName<TestType>(m => m.PublicBoolMethod());
+            bool result = testObj.Invoke<TestType, bool>(name);
+            Assert.True(testObj.Flag); // method is executed
+            Assert.True(result); // result is returned
+        }
+
+        [Fact]
+        public void ThrowsException_WhenMember_IsNot_Method()
+        {
+            Assert.Throws<ArgumentException>(() => typeof (TestType).GetName<TestType, int>(m => m.PublicProperty));
+        }
+
+        [Fact]
+        public void ThrowsException_WhenMember_ParametersNotMatch()
+        {
+            TestType testObj = new TestType();
+            string name = typeof (TestType).GetName<TestType>(m => m.PublicOverloadedMethod());
+            Assert.Throws<ArgumentException>(() => testObj.Invoke(name, string.Empty)); // there is no method with such parameters
+        }
+
+        [Fact]
+        public void ThrowsException_WhenMember_Absent()
+        {
+            TestType testObj = new TestType();
+            const string name = "MissingMethod";
+            Assert.Throws<ArgumentException>(() => testObj.Invoke(name)); // there is no such method
+        }
+
+        public class TestType
+        {
+            public bool Flag { get; private set; }
+            public int PublicProperty { get; set; }
+
+            public void PublicOverloadedMethod()
+            {
+                this.Flag = true;
+            }
+
+            public void PublicOverloadedMethod(int a)
+            {
+                this.Flag = true;
+            }
+
+            public void PublicOverloadedMethod(int a, string b)
+            {
+                this.Flag = true;
+            }
+
+            public bool PublicBoolMethod()
+            {
+                this.Flag = true;
+                return true;
+            }
+        }
+    }
+}
